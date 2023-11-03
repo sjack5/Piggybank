18d5a4a7ad30b43a79ab7e01c995ca1dd0b6432a
sjack5 <sjacklin01@gmail.com>
2023-11-02 21:38:28 -0600

    Pretty much the whole project LETS GOOOOOO

--- piggybank.py
+++ piggybank.py
@@ -1,10 +1,38 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
+import sqlite3
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/static')
 
 @app.route('/')
-def index():
-    return render_template('index.html')
+def display_data():
+    conn = sqlite3.connect("PiggybankDatabase.db")
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM Piggybank")
+    data = cursor.fetchall()
+    conn.close()
+    return render_template('index.html', data=data)
 
+@app.route('/add_row', methods=['POST'])
+def add_row():
+    if request.method == 'POST':
+        new_data = (
+            request.form['TransactionID'],
+            request.form['TransactionDate'],
+            request.form['TransactionCategory'],
+            request.form['TransactionName'],
+            request.form['TransactionPrice']
+        )
+        
+        conn = sqlite3.connect("PiggybankDatabase.db")
+        cursor = conn.cursor()
+        cursor.execute("INSERT INTO Piggybank (ID, Date, 'Transaction Category', 'Transaction Name', 'Transaction Amount') VALUES (?, ?, ?, ?, ?)", new_data)
+        conn.commit()
+        conn.close()
+        
+        # Redirect back to the main page after adding data to the database
+        return redirect('/')
+    
+    return redirect('/')
+    
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
